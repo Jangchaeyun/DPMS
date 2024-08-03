acf02e4f7b69570c1320e63b19b7a9e2fdc4d886
Jangchaeyun <zzangsally@naver.com>
2024-08-03 22:55:11 +0900

    0803 modal

--- BackEnd/src/main/java/com/day/projectmanagementsystem/config/AppConfig.java
+++ BackEnd/src/main/java/com/day/projectmanagementsystem/config/AppConfig.java
@@ -39,9 +39,9 @@ public class AppConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration cfg = new CorsConfiguration();
                 cfg.setAllowedOrigins(Arrays.asList(
-                        "http://localhost:3000/",
-                        "http://localhost:5173/",
-                        "http://localhost:4200/"
+                        "http://localhost:3000",
+                        "http://localhost:5173",
+                        "http://localhost:4200"
                 ));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
                 cfg.setAllowCredentials(true);
