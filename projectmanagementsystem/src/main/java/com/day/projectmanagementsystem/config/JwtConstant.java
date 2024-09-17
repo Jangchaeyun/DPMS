62822e332511d869d4e34cc4d172c2a78ce69813
Jangchaeyun <zzangsally@naver.com>
2024-09-17 15:37:43 +0900

    0917 JwtToken

---
+++ projectmanagementsystem/src/main/java/com/day/projectmanagementsystem/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.day.projectmanagementsystem.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "dfscfdgfregrvrwjoeiwjcoenc dfkewpofocjoriejfohciur owdjfeowcjooerijg3r";
+    public static final String JWT_HEADER = "Authorization";
+}
