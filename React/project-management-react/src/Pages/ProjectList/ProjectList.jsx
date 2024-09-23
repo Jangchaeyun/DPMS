1f0a4bb39d09e9bca2bb04bddc403ca8dc83135a
Jangchaeyun <zzangsally@naver.com>
2024-09-23 13:49:52 +0900

    0923 HomePage

---
+++ React/project-management-react/src/Pages/ProjectList/ProjectList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <>
+      <div className="relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5"></div>
+    </>
+  );
+};
+
+export default ProjectList;
